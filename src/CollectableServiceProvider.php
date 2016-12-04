348c52932d1517ee2cf4ce9c8cd97362fd112157
Brian Faust <hello@brianfaust.de>
2016-12-04 11:54:41 +0200

    Fix extending service provider name

--- src/CollectableServiceProvider.php
+++ src/CollectableServiceProvider.php
@@ -22,9 +22,9 @@ declare(strict_types=1);
 
 namespace BrianFaust\Collectable;
 
-use BrianFaust\ServiceProvider\ServiceProvider;
+use BrianFaust\ServiceProvider\AbstractServiceProvider;
 
-class CollectableServiceProvider extends ServiceProvider
+class CollectableServiceProvider extends AbstractServiceProvider
 {
     /**
      * Bootstrap the application services.
